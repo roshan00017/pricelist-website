6b1fea7c150e7f5944557e2b085a2e014c659c02
roshan <roshangyawali17@gmail.com>
2025-08-19 09:57:25 +0545

    update model

--- backend/migrations/20250816105336-create-pricelist.js
+++ backend/migrations/20250816105336-create-pricelist.js
@@ -1,40 +1,53 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Pricelists', {
+    await queryInterface.createTable("Pricelists", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       product_service: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       in_price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       vat_rate: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       row_type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+      },
+      unit: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      in_stock: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Pricelists');
-  }
+    await queryInterface.dropTable("Pricelists");
+  },
 };
--- backend/models/pricelist.model.js
+++ backend/models/pricelist.model.js
@@ -1,7 +1,9 @@
 "use strict";
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Pricelist extends Model {}
+
   Pricelist.init(
     {
       product_service: DataTypes.STRING,
@@ -9,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
       price: DataTypes.DECIMAL,
       vat_rate: DataTypes.DECIMAL,
       row_type: DataTypes.STRING,
+      unit: DataTypes.STRING,
+      in_stock: DataTypes.INTEGER,
+      description: DataTypes.TEXT,
     },
     {
       sequelize,
