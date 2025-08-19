a3d47ce113540422fe6f3fd015fe44e47e8c1eb4
roshan <roshangyawali17@gmail.com>
2025-08-19 10:18:41 +0545

    fic url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log("url", API_BASE_URL);
 
 export const fetchPricelist = async () => {
   try {
